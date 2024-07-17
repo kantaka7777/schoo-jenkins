53643f5ed74ae00d1fce184854d4feac5783d8de
kantaka7777 <kantaka7777poke@gmail.com>
2024-07-17 15:57:27 +0900

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
+chcp 65001
 cd "%~pd0"
 mkdir target\classes
 javac -cp "lib\*" -d "target\classes" src/main/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/runner/*.java
